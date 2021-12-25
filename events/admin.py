4f39c18c44f94aec37acb9423bc30f979244a360
Andrey <varioustemp@gmail.com>
2021-12-25 16:55:16 +0300

    admin filters added

--- events/admin.py
+++ events/admin.py
@@ -1,13 +1,93 @@
 from django.contrib import admin
 
-from .models import Category, Event
+from .models import (Category, Feature,
+                     Enroll, Event, Review,
+                     EVENT_SOLD_OUT,
+                     EVENT_SOLD_GREATER_THAN_50_PERCENT,
+                     EVENT_SOLD_LESS_OR_EQUAL_THAN_50_PERCENT)
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 0
+    can_delete = False
+    readonly_fields = [field.name for field in Review._meta.fields
+                       if field.name != 'id']
+
+    def has_add_permission(self, request, obj):
+        return False
+
+
+class EventSoldFilter(admin.SimpleListFilter):
+    title = 'Заполненность'
+    parameter_name = 'event_sold_filter'
+
+    def lookups(self, request, model_admin):
+        filter_list = (
+            ('0', EVENT_SOLD_OUT),
+            ('1', EVENT_SOLD_LESS_OR_EQUAL_THAN_50_PERCENT),
+            ('2', EVENT_SOLD_GREATER_THAN_50_PERCENT)
+        )
+        return filter_list
+
+    def queryset(self, request, queryset):
+        filter_value = self.value()
+        if filter_value == '0':
+            sold_out_events_ids = [event.id for event in queryset if
+                                   event.display_places_left() == EVENT_SOLD_OUT]
+            return queryset.filter(id__in=sold_out_events_ids)
+        elif filter_value == '1':
+            sold_less_or_equal_ids = [event.id for event in queryset if
+                                      event.participants_number / 2 >= event.display_enroll_count()]
+            return queryset.filter(id__in=sold_less_or_equal_ids)
+        elif filter_value == '2':
+            sold_greater_than_ids = [event.id for event in queryset if
+                                     event.participants_number / 2 < event.display_enroll_count()
+                                     and event.participants_number - event.display_enroll_count()]
+            return queryset.filter(id__in=sold_greater_than_ids)
+        return queryset
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'display_event_count']
+    list_display_links = ['id', 'title']
+
+
+@admin.register(Feature)
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_display_links = ['id', 'title']
+
+
+@admin.register(Enroll)
+class EnrollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'event', 'created']
+    list_select_related = ['user', 'event']
+    list_display_links = ['id', 'user', 'event']
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'date_start',
+                    'is_private', 'category', 'participants_number',
+                    'display_enroll_count', 'display_places_left']
+    list_select_related = ['category']
+    ordering = ['date_start']
+    search_fields = ['title']
+    fields = ([field.name for field in Event._meta.fields if field.name != 'id'] +
+              ['display_enroll_count', 'display_places_left', 'features'])
+    readonly_fields = ['display_enroll_count', 'display_places_left']
+    filter_horizontal = ['features']
+    list_filter = [EventSoldFilter, 'category', 'features']
+    inlines = [ReviewInline]
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'event', 'created']
+    list_display_links = ['id', 'user', 'event']
+    list_select_related = ['user', 'event']
+    list_filter = ['event', 'created']
+    fields = ['id', 'created', 'updated']
+    readonly_fields = ['created', 'updated', 'id', ]
--- events/models.py
+++ events/models.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator
 from django.db import models
+
+
+EVENT_SOLD_OUT = '0 (sold-out)'
+EVENT_SOLD_LESS_OR_EQUAL_THAN_50_PERCENT = '(<= 50%)'
+EVENT_SOLD_GREATER_THAN_50_PERCENT = '(> 50%)'
 
 
 class Event(models.Model):
@@ -13,13 +19,34 @@ class Event(models.Model):
         validators=[MaxValueValidator(10000)],
         verbose_name='Количество участников')
     is_private = models.BooleanField(default=False, verbose_name='Частное')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE,
+                                 related_name='events',
+                                 verbose_name='Категория')
+    features = models.ManyToManyField('Feature', verbose_name='Свойства событий')
 
     class Meta:
+        db_table = 'events'
         verbose_name = 'Событие'
         verbose_name_plural = 'События'
 
     def __str__(self):
         return self.title
+
+    def display_enroll_count(self):
+        return self.enrolls.count()
+
+    display_enroll_count.short_description = 'Количество записей'
+
+    def display_places_left(self):
+        signed_participants = self.display_enroll_count()
+        if not (self.participants_number - signed_participants):
+            return EVENT_SOLD_OUT
+        elif self.participants_number / 2 >= signed_participants:
+            return f"{self.participants_number - signed_participants} {EVENT_SOLD_LESS_OR_EQUAL_THAN_50_PERCENT}"
+        elif 0 < self.participants_number / 2 < signed_participants:
+            return f"{self.participants_number - signed_participants} {EVENT_SOLD_GREATER_THAN_50_PERCENT}"
+
+    display_places_left.short_description = 'Осталось мест'
 
 
 class Category(models.Model):
@@ -29,8 +56,69 @@ class Category(models.Model):
         verbose_name='Категория')
 
     class Meta:
+        db_table = 'categories'
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
+    def display_event_count(self):
+        return self.events.count()
+
     def __str__(self):
         return self.title
+
+
+class Feature(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'features'
+        verbose_name = 'Свойство события'
+        verbose_name_plural = 'Свойства событий'
+
+
+class Enroll(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             verbose_name='Пользователь',
+                             related_name='enrolls')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE,
+                              verbose_name='События',
+                              related_name='enrolls')
+    created = models.DateTimeField(auto_now_add=True,
+                                   verbose_name='Создано')
+
+    def __str__(self):
+        return self.event.title
+
+    class Meta:
+        db_table = 'enrolls'
+        verbose_name = 'Запись на событие'
+        verbose_name_plural = 'Записи на события'
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             verbose_name='Пользователь',
+                             related_name='reviews')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE,
+                              verbose_name='События',
+                              related_name='reviews')
+    rate = models.PositiveSmallIntegerField(verbose_name='Оценка пользователя')
+    text = models.TextField(verbose_name='Текст отзыва')
+    created = models.DateTimeField(auto_now_add=True,
+                                   verbose_name='Создано')
+    updated = models.DateTimeField(auto_now=True,
+                                   verbose_name='Изменено')
+
+    def __str__(self):
+        return self.event.title
+
+    class Meta:
+        db_table = 'reviews'
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
