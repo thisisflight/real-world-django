ba4c04072c3f02bee459d2e5caf3b2f49fbafc20
Andrey <varioustemp@gmail.com>
2022-02-19 12:49:28 +0300

    dev, preprod and prod settings added

--- realworlddjango/settings_preprod.py
+++ realworlddjango/settings_preprod.py
@@ -1,19 +1,9 @@
-from .settings_base import *
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-DEBUG = False
+from .settings_base import *
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': '',
-    }
-}
+DEBUG = False
 
 sentry_sdk.init(
     dsn=os.environ.get("DSN_TOKEN"),
